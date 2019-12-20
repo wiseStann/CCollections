6b0498c381979b663e01a3060460d5640111b762
StarKer <45110181+StarKerrr@users.noreply.github.com>
2019-12-20 15:24:55 +0300

    Update array.h

--- include/array.h
+++ include/array.h
@@ -8,6 +8,8 @@
 
 #define STANDARD_EXPANSION_VAL 1.5
 #define STANDARD_CAPACITY 10
+
+// Denote maximum capacity for the queue just for controlling size of allocated memory
 #define MAXSIZE INT_MAX
 
 
